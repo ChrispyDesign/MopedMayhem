52d6f042e7d0ace45c317af17076c1d4badf608e
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2018-09-19 17:37:54 +1000

    Added ProBuilder, Shaderforge and Trigger Display packages.

---
+++ Moped Mayhem v1.0/Assets/BasicScripts/TriggerMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TriggerDisplay))]
+public class TriggerMessage : MonoBehaviour {
+
+	public string messageToShow = "Default Message";
+	public float messageDuration = 3;
+
+	public string requiresItem = "";
+
+	//Runs when the player moves into this trigger
+	void OnTriggerEnter(Collider other)
+	{
+		//If this trigger requires an item then check to see if the player has that item
+		if (!GameObject.FindObjectOfType<InventoryController> ().CheckItem (requiresItem)) {
+			//If they don't, stop running this funtion
+			return;
+		}
+
+		//Checks if the tag of the object entered is "Player"
+		if (other.tag == "Player") {
+			GameObject.FindObjectOfType<UIController> ().
+				ShowMessage (messageToShow, messageDuration);
+		}
+	}
+}
---
+++ Moped Mayhem v1.0/Assets/BasicScripts/TriggerPlaySound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlaySound : MonoBehaviour {
+
+	public AudioClip clip;
+	public float interactDelay = 0;
+
+	//Runs when the player moves into this trigger
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			//Run the function "PlaySound" after [interactDelay] seconds
+			Invoke("PlaySound", interactDelay);
+		}
+	}
+
+	private void PlaySound()
+	{
+		AudioSource.PlayClipAtPoint (clip, transform.position);
+	}
+
+}
