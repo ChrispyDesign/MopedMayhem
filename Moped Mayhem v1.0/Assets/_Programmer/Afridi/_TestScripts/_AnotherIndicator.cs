c38c0f4ef7e19d73d61f062b60963d0b31005cb4
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2018-10-10 17:32:38 +1100

    - Camera is Orthographic

---
+++ Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_AnotherIndicator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _AnotherIndicator : MonoBehaviour {
+
+    bool bCurrentlyHoldingFoodl = true;
+    public List<GameObject> numberofShops = new List<GameObject>();
+
+    void Update()
+    {
+        if (bCurrentlyHoldingFoodl == true) {
+            foreach (var shop in numberofShops) {
+
+            }
+
+        }
+    }
+}
--- Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_TestMovement.cs
+++ Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_TestMovement.cs
@@ -4,41 +4,12 @@ using UnityEngine;
 
 public class _TestMovement : MonoBehaviour {
 
-
-    //creates a variable to store the character controller component we just made but doesn’t expose it in the inspector because we made it private.
-    private CharacterController control;
-    public float speed = 5.0f;
-
-    // Use this for initialization
-    void Start()
-    {
-        //gets the CharacterController sub component of this object and assigns it to our controller variable
-        control = gameObject.GetComponent<CharacterController>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 80.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
 
-        //Will Move the Character in the direction of the vector we pass it into
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            control.transform.Translate(Vector3.back * speed);
-        }
-
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            control.transform.Translate(Vector3.right * speed); ;
-        }
-
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            control.transform.Translate(Vector3.forward * speed);
-        }
-
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            control.transform.Translate(Vector3.left * speed);
-        }
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
     }
 }
