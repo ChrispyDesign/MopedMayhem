54c84dd41599711e9c3f30212bd8a72266acf2e6
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2018-10-11 10:03:28 +1100

    - Finished Alpha Delivery Indicator

--- Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_AnotherIndicator.cs
+++ Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_AnotherIndicator.cs
@@ -1,19 +1,125 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class _AnotherIndicator : MonoBehaviour {
 
-    bool bCurrentlyHoldingFoodl = true;
-    public List<GameObject> numberofShops = new List<GameObject>();
+    private Camera mainCamera;
+    private RectTransform m_icon;
+    private Image m_iconImage;
+    private Canvas mainCanvas;
+
+    private Vector3 m_cameraOffsetUp;
+    private Vector3 m_cameraOffsetRight;
+    private Vector3 m_cameraOffsetForward;
+
+    public Sprite m_targetIconOnScreen;
+    public Sprite m_targetIconOffScreen;
+
+    [Space]
+
+    [Range(0, 100)]
+    public float m_edgeBuffer;
+    public Vector3 m_targetIconScale;
+
+    [Space]
+    public bool isOffScreen;
+    public bool ShowDebugLines;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        mainCanvas = FindObjectOfType<Canvas>();
+        Debug.Assert((mainCanvas != null), "There needs to be a Canvas object in the scene for the OTI to display");
+
+        InstainateTargetIcon();
+    }
 
     void Update()
     {
-        if (bCurrentlyHoldingFoodl == true) {
-            foreach (var shop in numberofShops) {
+        if (ShowDebugLines)
+            DrawDebugLines();
 
-            }
+        UpdateTargetIconPosition();
+    }
 
+    private void InstainateTargetIcon()
+    {
+        m_icon = new GameObject().AddComponent<RectTransform>();
+        m_icon.transform.SetParent(mainCanvas.transform);
+        m_icon.localScale = m_targetIconScale;
+
+        m_icon.name = name + "White Arrow";
+
+        m_iconImage = m_icon.gameObject.AddComponent<Image>();
+        m_iconImage.sprite = m_targetIconOnScreen;
+    }
+
+    private void UpdateTargetIconPosition()
+    {
+        Vector3 newPos = transform.position;
+
+        newPos = mainCamera.WorldToViewportPoint(newPos);
+
+        if (newPos.z < 0)
+        {
+            newPos.x = 1f - newPos.x;
+            newPos.y = 1f - newPos.y;
+            newPos.z = 1f - newPos.z;
+
+            newPos = Vector3Maxamize(newPos);
+            m_iconImage.sprite = m_targetIconOffScreen;
         }
+        else
+        {
+            m_iconImage.sprite = m_targetIconOnScreen;
+        }
+
+        newPos = mainCamera.ViewportToScreenPoint(newPos);
+
+        newPos.x = Mathf.Clamp(newPos.x, m_edgeBuffer, Screen.width - m_edgeBuffer);
+        newPos.y = Mathf.Clamp(newPos.y, m_edgeBuffer, Screen.height - m_edgeBuffer);
+
+        m_icon.transform.position = newPos;
+    }
+
+    public void DrawDebugLines()
+    {
+        Vector3 directionFromCamera = transform.position - mainCamera.transform.position;
+
+        Vector3 cameraForwad = mainCamera.transform.forward;
+        Vector3 cameraRight = mainCamera.transform.right;
+        Vector3 cameraUp = mainCamera.transform.up;
+
+        cameraForwad *= Vector3.Dot(cameraForwad, directionFromCamera);
+        cameraRight *= Vector3.Dot(cameraRight, directionFromCamera);
+        cameraUp *= Vector3.Dot(cameraUp, directionFromCamera);
+
+        Debug.DrawRay(mainCamera.transform.position, directionFromCamera, Color.magenta);
+
+        Vector3 forwardPlaneCenter = mainCamera.transform.position + cameraForwad;
+
+        Debug.DrawLine(mainCamera.transform.position, forwardPlaneCenter, Color.blue);
+        Debug.DrawLine(forwardPlaneCenter, forwardPlaneCenter + cameraUp, Color.green);
+        Debug.DrawLine(forwardPlaneCenter, forwardPlaneCenter + cameraRight, Color.red);
+    }
+
+
+
+    public Vector3 Vector3Maxamize(Vector3 vector)
+    {
+        Vector3 returnVector = vector;
+
+        float max = 0;
+
+        max = vector.x > max ? vector.x : max;
+        max = vector.y > max ? vector.y : max;
+        max = vector.z > max ? vector.z : max;
+
+        returnVector /= max;
+
+        return returnVector;
     }
 }
