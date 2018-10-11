7c0999853722b1a7961ab35eb9f29da810f07f14
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2018-10-11 23:46:55 +1100

    updated Greybox_ChrisB_002_GIFS_ChrisP.unity with the delivery indicator, minimap, demolisher scripts and functionality for GIFs.

--- Moped Mayhem v1.0/Assets/_Programmer/Chris/Scripts/Enemy/Demolisher/DemolisherTempMove.cs
+++ Moped Mayhem v1.0/Assets/_Programmer/Chris/Scripts/Enemy/Demolisher/DemolisherTempMove.cs
@@ -22,27 +22,29 @@ public class DemolisherTempMove : MonoBehaviour {
 	}
 	void Update ()
 	{
-		if (bStartLerp)
-		{
-			fEndTime = Time.realtimeSinceStartup + fLerpTime;
-			bStartLerp = false;
-			bLerping = true;
-			Debug.Log(fEndTime);
-		}
+        if (Input.GetKeyUp(KeyCode.Space))
+            bStartLerp = true;
+        if (bStartLerp)
+        {
+            fEndTime = Time.realtimeSinceStartup + fLerpTime;
+            bStartLerp = false;
+            bLerping = true;
+            Debug.Log(fEndTime);
+        }
 
-		if (bLerping)
-		{
-			if (fEndTime > Time.realtimeSinceStartup)
-			{
-				// was dividing by fEndTime instead of fLerpTime - want to get the percentage through the lerp time!
-				float fLerp = 1 - ((fEndTime - Time.realtimeSinceStartup) / fLerpTime);
+        if (bLerping)
+        {
+            if (fEndTime > Time.realtimeSinceStartup)
+            {
+                // was dividing by fEndTime instead of fLerpTime - want to get the percentage through the lerp time!
+                float fLerp = 1 - ((fEndTime - Time.realtimeSinceStartup) / fLerpTime);
 
-				transform.position = Vector3.Lerp(StartPos.position, EndPos.position, fLerp);
-			}
-			else
-			{
-				bLerping = false;
-			}
-		}
+                transform.position = Vector3.Lerp(StartPos.position, EndPos.position, fLerp);
+            }
+            else
+            {
+                bLerping = false;
+            }
+        }
 	}
 }
