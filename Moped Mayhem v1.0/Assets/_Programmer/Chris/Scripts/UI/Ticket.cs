ad4e6b7a4706db3861f86df0d9c488d47b05d29d
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2018-10-17 17:35:44 +1100

    Added Ticket.cs - Started script
    Added TicketManager.cs
    - Started script

---
+++ Moped Mayhem v1.0/Assets/_Programmer/Chris/Scripts/UI/Ticket.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ticket : MonoBehaviour {
+
+	public RawImage m_TicketImage;
+	public RawImage m_FoodImage;
+	private Color m_TicketColor;
+
+	public Slider m_Slider;
+	public Image m_Fill;
+
+	public Color m_FullColour;
+	public Color m_MidColour;
+	public Color m_EmptyColour;
+
+
+	private float m_fEndTime;
+	private float m_fDuration;
+
+	[HideInInspector]
+	public int m_nTicketPosition;
+
+	public void Setup(RawImage foodImage, Color ticketColor, float fDuration)
+	{
+		m_FoodImage = foodImage;
+		m_TicketImage.color = ticketColor;
+
+		m_fDuration = fDuration;
+		m_fEndTime = Time.realtimeSinceStartup + fDuration;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float currentTime = Time.realtimeSinceStartup;
+
+		float fLerp = 1 - ((m_fEndTime - currentTime) / m_fDuration);
+
+		m_Slider.value = Mathf.Lerp(1.0f, 0.0f, fLerp);
+
+
+		// change color
+		Color color = m_Fill.color;
+		if (fLerp < 0.5f)
+		{
+			float fColorLerp = (fLerp * 2);
+			color = Color.Lerp(m_FullColour, m_MidColour, fColorLerp);
+		}
+		else if (fLerp > 0.5f)
+		{
+			float fColorLerp = ((fLerp - 0.5f) * 2);
+			color = Color.Lerp(m_MidColour, m_EmptyColour, fColorLerp);
+		}
+
+		m_Fill.color = color;
+	}
+}
---
+++ Moped Mayhem v1.0/Assets/_Programmer/Chris/Scripts/UI/TicketManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicketManager : MonoBehaviour {
+
+	public GameObject m_TicketPrefab;
+	public Transform[] m_TicketPositions;
+
+	public float m_fOffscreenOffset;
+
+	private bool[] m_bTakenPostions;
+	private List<Ticket> m_ActiveTickets = new List<Ticket>();
+	private List<Ticket> m_InactiveTickets = new List<Ticket>();
+
+	// Use this for initialization
+	void Start ()
+	{
+		int nTicketPositionCount = m_TicketPositions.Length;
+		m_bTakenPostions = new bool[nTicketPositionCount];
+
+		for (int i = 0; i < nTicketPositionCount + 1; ++i)
+		{
+			Ticket tempTicket = Instantiate(m_TicketPrefab).GetComponent<Ticket>();
+			m_InactiveTickets.Add(tempTicket);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	private void Enter(Ticket ticket)
+	{
+		//if (endLerpTime == 0)
+		//{
+		//	endLerpTime = Time.realtimeSinceStartup + lerpTime;
+		//}
+		//else
+		//{
+		//	float fLerpTime = 1 - ((endLerpTime - currentTime) / lerpTime);
+		//	gameObject.transform.position = Vector3.Lerp(offPos, pos, fLerpTime);
+
+		//	if (gameObject.transform.position == pos)
+		//	{
+
+		//		endLerpTime = 0.0f;
+		//		entering = false;
+		//	}
+		//}
+	}
+
+	private void Exit(Ticket ticket)
+	{
+		//if (endLerpTime == 0)
+		//{
+		//	endLerpTime = Time.realtimeSinceStartup + lerpTime;
+		//}
+		//else
+		//{
+		//	float fLerpTime = 1 - ((endLerpTime - currentTime) / lerpTime);
+		//	gameObject.transform.position = Vector3.Lerp(pos, offPos, fLerpTime);
+
+		//	if (gameObject.transform.position == offPos)
+		//	{
+		//		endLerpTime = 0.0f;
+		//		Deactivate();
+		//		exiting = false;
+		//	}
+		//}
+	}
+
+	private void MoveDown(Ticket ticket)
+	{
+		//if (endLerpTime == 0)
+		//{
+		//	endLerpTime = Time.realtimeSinceStartup + lerpTime;
+		//}
+		//else
+		//{
+		//	float fLerpTime = 1 - ((endLerpTime - currentTime) / lerpTime);
+		//	gameObject.transform.position = Vector3.Lerp(pos, offPos, fLerpTime);
+
+		//	if (gameObject.transform.position == offPos)
+		//	{
+		//		endLerpTime = 0.0f;
+		//		Deactivate();
+		//		exiting = false;
+		//	}
+		//}
+	}
+
+	public void ActivateTicket(Order order)
+	{
+
+	}
+}
