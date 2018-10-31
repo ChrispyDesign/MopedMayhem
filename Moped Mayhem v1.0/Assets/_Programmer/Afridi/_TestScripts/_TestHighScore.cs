18a9e2e7c8905120c23d67e4e50852e1f28a375c
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2018-10-31 17:14:21 +1100

    - Main Menu UI V001 Uploaded

---
+++ Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_TestHighScore.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class _TestHighScore : MonoBehaviour {
+
+    public int[] scoreArray;
+    public int highscoreCount = 5;
+
+    public string currentDirectory;
+
+    public string scoreFileName = "MMHScore.txt";
+
+    private void Start()
+    {
+        //To double check, print the current directory
+        currentDirectory = Application.dataPath;
+        Debug.Log("The current directory is: " + currentDirectory);
+
+        //Call load scores function
+        LoadScoresFromFile();
+    }
+
+    public void LoadScoresFromFile()
+    {
+
+        //Before reading from a file, check it exists. If it doesn't exist, log a error message and abort
+        bool fileExists = File.Exists(currentDirectory + "/" + scoreFileName);
+        if (fileExists)
+        {
+            Debug.Log("Found highscore file " + scoreFileName);
+        }
+        else
+        {
+            Debug.LogWarning("The file " + scoreFileName + " does not exist. No scores will be loaded.");
+            SaveScoresToFile();
+            return;
+        }
+
+        //Make a new array of default values. This ensure no old values stick around if we've loaded a
+        //new scores file in the past
+        scoreArray = new int[highscoreCount];
+
+        //Reads the file in
+        StreamReader fileReader = new StreamReader(currentDirectory + "/" + scoreFileName);
+
+        //A counter to make sure we don't go past the end of our scores
+        int scoreCount = 0;
+
+        //While loop that runs as long as there is data to be read and as long as we haven't reached the
+        //end of our scores array
+        while (fileReader.Peek() != 0 && scoreCount < scoreArray.Length)
+        {
+            //Read the line into a variable
+            string fileLine = fileReader.ReadLine();
+
+            //Try to parse the variable into an integer
+            int readScore = -1;
+
+            //Try to parse it
+            bool didparse = int.TryParse(fileLine, out readScore);
+            if (didparse)
+            {
+                scoreArray[scoreCount] = readScore;
+            }
+            else
+            {
+                Debug.LogWarning("Invalid line in scores file at " + scoreCount + ", using default value.");
+                scoreArray[scoreCount] = 0;
+            }
+
+            //Increment counter
+            scoreCount++;
+        }
+
+        //Close the stream
+        fileReader.Close();
+        Debug.Log("Highscores read from " + scoreFileName);
+    }
+
+    public void SaveScoresToFile()
+    {
+        //Create a StreamWriter for our filepath
+        StreamWriter fileWriter = new StreamWriter(currentDirectory + "/" + scoreFileName);
+
+        //Write the line to the file
+        for (int i = 0; i < scoreArray.Length; i++)
+        {
+            fileWriter.WriteLine(scoreArray[i]);
+        }
+
+        //Close the stream
+        fileWriter.Close();
+
+        //Write a log message
+        Debug.Log("Highscores written to " + scoreFileName);
+    }
+
+    public void AddScore(int newScore)
+    {
+        //Find the index where the score will sit
+        int desiredIndex = -1;
+
+        for (int i = 0; i < scoreArray.Length; i++)
+        {
+            if (scoreArray[i] < newScore || scoreArray[i] == 0)
+            {
+                desiredIndex = i;
+                break;
+            }
+        }
+
+        //If no desired index was found then the score ins't high enough to get into the top 10
+        //so we just abort
+        if (desiredIndex < 0)
+        {
+            Debug.Log("Score of " + newScore + " is not high enough for high scores list.");
+            return;
+        }
+
+        //Move all scores below the one added down a position
+        for (int i = scoreArray.Length - 1; i > desiredIndex; i--)
+        {
+            scoreArray[i] = scoreArray[i - 1];
+        }
+
+        //Insert the new score in
+        scoreArray[desiredIndex] = newScore;
+        Debug.Log("Score of " + newScore + " entered into highscores at position " + desiredIndex);
+    }
+}
--- Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_TestMenuManager.cs
+++ Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_TestMenuManager.cs
@@ -6,34 +6,100 @@ using UnityEngine.UI;
 
 public class _TestMenuManager : MonoBehaviour {
 
+    public GameObject MainMenu;
     public GameObject PlayButton;
-    public GameObject OptionsButton;
     public GameObject QuitButton;
+    public GameObject OptionsButton;
+    public GameObject OptionsBackButton;
+    public GameObject SoundButton;
+    public GameObject SoundBackButton;
+    public GameObject LeaderButton;
+    public GameObject LeaderBackButton;
+
+    public _TestHighScore h_Scores;
+    public GameObject LeaderBoardScreen;
+    public GameObject OptionsScreen;
+    public GameObject SoundScreen;
 
     public GameObject CurrentSelec;
     public Button CurrentButton;
 
     void Awake()
     {
+        h_Scores = FindObjectOfType<_TestHighScore>();
         CurrentSelec = PlayButton;
         CurrentButton = CurrentSelec.GetComponent<Button>();
     }
 
     void Update () {
-
-
-        QuitGame();
+        /*These Controls work with both PC and PS4 (Unsure for Xbox)*/
+        if (Input.GetButtonDown("Submit"))
+        {
+            if (CurrentSelec == PlayButton)
+            {
+                PlayGame();
+            }
+            else if (CurrentSelec == LeaderButton)
+            {
+                // Opens the highscores screen
+                HighScoreStore();
+                CurrentSelec = LeaderBackButton;
+            }
+            else if (CurrentSelec == LeaderBackButton)
+            {
+                // Closes the highscores screen
+                CurrentSelec = LeaderButton;
+                LeaderBoardScreen.SetActive(false);
+                MainMenu.SetActive(true);
+            }
+            else if (CurrentSelec == OptionsButton)
+            {
+                //Option Included Stuff
+                /*Sound (Music, Effects), Resolution, Invert Controls?, Customise Controls? or Schemes?, Mutator Settings*/
+            }
+            else if (CurrentSelec == OptionsBackButton)
+            {
+                CurrentSelec = OptionsButton;
+                OptionsScreen.SetActive(false);
+                MainMenu.SetActive(true);
+            }
+            else if (CurrentSelec == SoundButton)
+            {
+                SoundContents();
+                CurrentSelec = SoundBackButton;
+            }
+            else if (CurrentSelec == SoundBackButton) {
+                CurrentSelec = SoundButton;
+                SoundScreen.SetActive(false);
+                OptionsScreen.SetActive(true);
+            }
+            else if (CurrentSelec == QuitButton)
+            {
+                QuitGame();
+            }
+        }
 	}
 
-    void PlayGame() {
+    public void PlayGame() {
         SceneManager.LoadScene(1);
     }
 
-    void OptionsBtn() {
+    public void OptionContents() {
 
     }
 
-    void QuitGame() {
+    public void HighScoreStore() {
+        LeaderBoardScreen.SetActive(true);
+        MainMenu.SetActive(false);
+        h_Scores.LoadScoresFromFile();
+    }
+
+    public void SoundContents() {
+
+    }
+
+    public void QuitGame()
+    {
         // Quits the application if exe file
 #if UNITY_STANDALONE
         Application.Quit();
@@ -45,7 +111,4 @@ public class _TestMenuManager : MonoBehaviour {
 #endif
     }
 
-    void OptionContents() {
-
-    }
 }
