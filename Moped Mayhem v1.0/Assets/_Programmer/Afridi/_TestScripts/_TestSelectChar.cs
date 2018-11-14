22667f70cef544349441aa61f85bea3bdf73098e
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2018-11-15 09:27:52 +1100

    - Player Custom actually fixed

--- Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_TestSelectChar.cs
+++ Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_TestSelectChar.cs
@@ -5,39 +5,16 @@ using UnityEngine.UI;
 
 public class _TestSelectChar : MonoBehaviour {
 
-    public Toggle Male;
     public Toggle FeMale;
-    public GameObject MMoped;
-    public GameObject FMoped;
+    public bool isSwitching = false;
 
     public void Awake()
     {
-        MMoped = GameObject.Find("Player");
-        FMoped = GameObject.Find("Femin");
-    }
-
-    public void Start()
-    {
-        MMoped.SetActive(true);
-        FMoped.SetActive(false);
+        DontDestroyOnLoad(gameObject);
     }
 
     private void Update()
     {
-        PickPlayer();      
-    }
-
-    public void PickPlayer()
-    {
-        // afridi can suck salted nuts
-        if (Male.isOn) {
-            MMoped.SetActive(true);
-            FMoped.SetActive(false);
-        }
-
-        if (FeMale.isOn) {
-            MMoped.SetActive(false);
-            FMoped.SetActive(true);
-        }
+        isSwitching = FeMale.isOn;
     }
 }
