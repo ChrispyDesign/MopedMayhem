8d2af90ede89cb3f423dff1a26ee61a343c07e10
DGiroud <39175181+DGiroud@users.noreply.github.com>
2018-11-14 21:34:31 +1100

    Reorganised the prefab folder. Kept building the level.

---
+++ Moped Mayhem v1.0/Assets/CountGameObjects.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountGameObjects : MonoBehaviour {
+
+    public List<GameObject> numberOfGO;                 // list that stores all the game objects in the scene
+
+    private void Awake()
+    {
+        // adds all game objects to a list, then counts them and prints it in debug log
+        foreach (var item in FindObjectsOfType<GameObject>())
+        {
+            numberOfGO.Add(item);
+        }
+        Debug.Log(numberOfGO.Count);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    //private void OnDrawGizmos()
+    //{
+    //    Debug.LogError(numberOfGO.Count);
+    //}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
