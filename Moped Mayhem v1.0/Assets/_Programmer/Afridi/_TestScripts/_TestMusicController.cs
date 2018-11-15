5306f0fa056785cc48e91dc3cc04edc5dde5d438
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2018-11-16 09:56:57 +1100

    - Main Menu Finalised and Ready For Beta

---
+++ Moped Mayhem v1.0/Assets/_Programmer/Afridi/_TestScripts/_TestMusicController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _TestMusicController : MonoBehaviour {
+
+    private void Awake()
+    {
+        // Keeps the GameObject this is attached to persistent through scenes
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Music");
+        if (obj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
