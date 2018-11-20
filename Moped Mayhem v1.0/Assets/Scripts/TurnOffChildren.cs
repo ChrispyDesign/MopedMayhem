e73993c0e4e09ace366a9b7bef361b4be5ec718d
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2018-11-20 14:51:45 +1100

    Adjusted SniperSpawners. Create a Main_Gold_Chris_001.unity scene.

---
+++ Moped Mayhem v1.0/Assets/Scripts/TurnOffChildren.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffChildren : MonoBehaviour {
+
+    public GameObject thisGO;
+
+	// Use this for initialization
+	void Start () {
+        thisGO = this.gameObject;
+        thisGO.transform.GetChild(0).gameObject.SetActive(false);
+	}
+	
+}
