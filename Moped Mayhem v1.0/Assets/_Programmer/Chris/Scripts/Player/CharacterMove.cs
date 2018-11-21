a648b387b6c7793c1c2d7ca36ad9da2d14cdac12
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2018-11-22 00:21:45 +1100

    Modified CharacterMove.cs - Now has gravity
    Modified FIX_THIS.unity
    - Snipers now do knockback
    - Shows that CharacterController is a dead end... YAY...

--- Moped Mayhem v1.0/Assets/_Programmer/Chris/Scripts/Player/CharacterMove.cs
+++ Moped Mayhem v1.0/Assets/_Programmer/Chris/Scripts/Player/CharacterMove.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour {
 
 	private CharacterController m_Controller;
+	public Transform m_CenOfMass;
 
-	public float m_fSpeed = 10000.0f;
-	public float m_fRotSpeed = 1000.0f;
+	public float m_fSpeed = 100.0f;
+	public float m_fRotSpeed = 10.0f;
+
+	public float m_fGravity = 10.0f;
 
 	// Use this for initialization
 	void Start ()
@@ -21,11 +24,16 @@ public class CharacterMove : MonoBehaviour {
 		float fVer = Input.GetAxis("Vertical");
 		float fHor = Input.GetAxis("Horizontal");
 
-		m_Controller.Move(transform.forward * fVer * Time.fixedDeltaTime * m_fSpeed);
-
 		if (Mathf.Abs(fHor) > 0.1f)
 		{
-			transform.Rotate(transform.up * fHor * Time.fixedDeltaTime * m_fRotSpeed);
+			float fAngle = fHor * Time.fixedDeltaTime * m_fRotSpeed;
+			transform.RotateAround(m_CenOfMass.position, transform.up, fAngle);
 		}
+
+		Vector3 v3Movement = transform.forward * fVer * Time.fixedDeltaTime * m_fSpeed;
+		v3Movement.y = -m_fGravity * Time.fixedDeltaTime;		
+
+
+		m_Controller.Move(v3Movement);
 	}
 }
