67e0a1949581598143a0fcc51a7654a7c64e6c29
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2018-11-22 20:57:07 +1100

    Created a new scene for working on lighting and post-processing.

---
+++ Moped Mayhem v1.0/Assets/Scripts/TEST_TurnOffOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_TurnOffOnCollision : MonoBehaviour {
+
+    public List<GameObject> m_ThingsToTurnOff;                 // list that stores all that you want to turn off
+
+    // when the player collides with it, turn all those things off
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (var obj in m_ThingsToTurnOff)
+        {
+            obj.gameObject.SetActive(false);
+        }
+    }
+}
