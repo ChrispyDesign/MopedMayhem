7aa6b8123330799fea8e2f428a4eb29fd6b0b502
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2018-11-23 16:19:44 +1100

    Moved all the minimap UI elements higher up, so they don't interfere.
    You will have to move your Minimap Camera Large and Minimap Camera Small up to Y: 1000.

---
+++ Moped Mayhem v1.0/Assets/Scripts/RotateTowards.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowards : MonoBehaviour {
+
+    // The object whose rotation we want to match.
+    public Transform target;
+
+    // Angular speed in degrees per sec.
+    public float speed;
+
+    void Update()
+    {
+        // The step size is equal to speed times frame time.
+        var step = speed * Time.deltaTime;
+
+        // Rotate our transform a step closer to the target's.
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step);
+    }
+}
