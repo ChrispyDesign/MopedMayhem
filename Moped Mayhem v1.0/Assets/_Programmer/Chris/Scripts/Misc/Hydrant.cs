f2c3096d35ab11735041305aaff6bd6ad669464b
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2018-11-23 16:56:07 +1100

    Addded Hydrant.cs - Helper Script for fire Hydrant - makes particles play on initial collision
    Modified Prop_FireHydrant.prefab
    - Now has particles attached

---
+++ Moped Mayhem v1.0/Assets/_Programmer/Chris/Scripts/Misc/Hydrant.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hydrant : MonoBehaviour {
+
+	public ParticleContainer HydrantParticles;
+	private bool m_bHit = false;
+
+	private void Awake()
+	{
+		transform.DetachChildren();
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (!m_bHit)
+		{
+			var tag = collision.collider.tag;
+			if (tag == "Player" || tag == "Biker")
+			{
+				HydrantParticles.Play();
+				HydrantParticles.m_bCanDie = true;
+				m_bHit = true;
+			}
+		}
+	}
+}
