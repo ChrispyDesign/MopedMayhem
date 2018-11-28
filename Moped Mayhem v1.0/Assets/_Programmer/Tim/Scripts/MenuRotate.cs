d8eb4b5e5a3e2fa722630ce9088ec8115d3e6c99
Kazwyrm <tlangford203@gmail.com>
2018-11-28 17:38:35 +1100

    Added MenuRotate.cs - rotates around a point to show the world for the Menu

---
+++ Moped Mayhem v1.0/Assets/_Programmer/Tim/Scripts/MenuRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuRotate : MonoBehaviour {
+
+	private Vector3 MenuRot;
+	public float rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+		MenuRot.y = rotSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(MenuRot);
+	}
+}
