b5795964491bd31fa28969e9c727e55144c2d25e
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2018-11-29 14:36:14 +1100

    Added Rotate.cs - Does what it says on the tin

---
+++ Moped Mayhem v1.0/Assets/_Programmer/Chris/Scripts/Misc/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public Vector3 m_Rotation;
+		
+	// Update is called once per frame
+	void Update ()
+	{
+		var rot = transform.rotation;
+		rot.eulerAngles += m_Rotation * Time.deltaTime;
+
+		transform.rotation = rot;
+	}
+}
